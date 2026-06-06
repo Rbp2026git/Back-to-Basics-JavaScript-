4de451d0a676a01fc195756e0aba92920ef7b85f
Raushan Bhai Patel <rbpmicrosoft2025@gmail.com>
2026-06-06 14:05:12 +0530

    project01 completed

---
+++ JavaScript/script3.js
@@ -0,0 +1,26 @@
+/*for (let i = 0; i <= 7; i++){
+    if(i === 5){
+        continue;
+    }
+    console.log(i);
+}*/
+
+// const user = 'Raushan Bhai Patel';
+// const age = 23;
+// const city = "Patna";
+// const balance = 1500.75;
+
+// const msg = `Hello, ${user}!
+// You are ${age} years old and located in ${city}.
+// Your current wallet balance is ₹${balance}.`;
+
+// const msg2 = "Hello, " + user + " ! You are " + age + " years old and located in " + city + ". Your current wallet balance is ₹ " + balance + ".";
+
+
+// const product = "laptop";
+// const price = "40000";
+// const quantity = 2;
+
+// console.log(`Total bill : ₹${price * quantity}`);
+// console.log(`Total bill(GST included) : ₹${price * quantity * 1.18}`);
+
